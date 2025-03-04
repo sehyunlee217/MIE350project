ac4ccb55b8a39803f6fabb6b47ad9a0c9d9e371b
sehyun <leeseunghyun217@gmail.com>
2025-03-04 10:05:15 -0500

    Added Alcohol superclass and init h2 db

--- cms/src/main/java/com/example/cms/model/entities/Alcohol.java
+++ cms/src/main/java/com/example/cms/model/entities/Alcohol.java
@@ -1,5 +1,29 @@
 package com.example.cms.model.entities;
 
-public class Alcohol {
-    
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotEmpty;
+
+@MappedSuperclass
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class Alcohol {
+
+    @Id
+    private long id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private long price;
+
+    @NotEmpty
+    private long amount;
 }
--- cms/src/main/java/com/example/cms/model/entities/Beer.java
+++ cms/src/main/java/com/example/cms/model/entities/Beer.java
@@ -1,5 +1,18 @@
 package com.example.cms.model.entities;
 
-public class Beer {
-    
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "Beer")
+public class Beer extends Alcohol{
+    private String beerType;
+    private String beerOrigin;
 }
